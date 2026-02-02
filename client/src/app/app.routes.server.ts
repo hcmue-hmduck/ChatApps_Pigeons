0e0a80bf587989f5d4444b98e5bfb362b4f8156f
HM Duck <hmduck@192.168.1.92>
2026-02-02 14:21:46 +0700

    Fix Error: Render

--- client/src/app/app.routes.server.ts
+++ client/src/app/app.routes.server.ts
@@ -2,7 +2,23 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: '',
+    renderMode: RenderMode.Prerender,
+  },
+  {
+    path: 'conversations/:id',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'admin/users',
+    renderMode: RenderMode.Prerender,
+  },
+  {
+    path: 'messages/:id',
+    renderMode: RenderMode.Server,
+  },
+  {
     path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    renderMode: RenderMode.Prerender,
+  },
 ];
