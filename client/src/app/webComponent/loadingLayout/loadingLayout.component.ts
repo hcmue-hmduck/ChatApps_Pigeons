cf1595a14aa5afadd73066ca93e1606610a08316
HMD-CVA <huynhmyduc2005@gmail.com>
2026-03-12 10:45:52 +0700

    UI: Loading Layout UI

---
+++ client/src/app/webComponent/loadingLayout/loadingLayout.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-loading-layout',
+    standalone: true,
+    imports: [],
+    templateUrl: './loadingLayout.component.html',
+    styleUrl: './loadingLayout.component.css'
+})
+export class LoadingLayoutComponent {
+
+}
