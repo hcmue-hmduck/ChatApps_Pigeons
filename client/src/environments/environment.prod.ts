85e3d27bbc1f876e63a0b96ceeee8c86d42ed7d2
HM Duck <hmduck@MacBook-Air-cua-HM.local>
2026-03-21 22:23:39 +0700

    Fix Bug Deploy Render: Missing ProvinceAPI on Production ENV

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -2,4 +2,5 @@ export const environment = {
     production: true,
     apiUrl: 'https://chatapps-pigeons-backend.onrender.com',
     livekit_wsUrl: 'wss://pigeons-wsz2aiur.livekit.cloud',
+    ProvinceAPI: 'https://provinces.open-api.vn/api/p/',
 };
