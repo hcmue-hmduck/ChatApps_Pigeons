afaae51052bf4b5819917bd211c849e3d8d21e5e
HM Duck <hmduck@192.168.1.121>
2026-03-26 05:13:10 +0700

    Fix Bug Render

--- client/src/app/app.routes.server.ts
+++ client/src/app/app.routes.server.ts
@@ -14,10 +14,6 @@ export const serverRoutes: ServerRoute[] = [
     renderMode: RenderMode.Prerender,
   },
   {
-    path: 'messages/:id',
-    renderMode: RenderMode.Client,
-  },
-  {
     path: 'call-display',
     renderMode: RenderMode.Client,
   },
